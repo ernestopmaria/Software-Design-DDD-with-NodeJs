a9b91d983f8163902ca893191c2bc4483230110a
ernestopmaria <ernestomaria93@gmail.com>
2023-12-03 16:33:00 -0600

    create entities and refactor them

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,14 +1,14 @@
-import { randomUUID } from "node:crypto";
+import { Slug } from "./value-objects/slug";
+import { Entity } from "../../core/entities/entity";
 
-export class Question {
-  public id: string;
-  public title: string;
-  public content: string;
+interface QuestionProps {
+  title: string, 
+  content: string, 
+  slug:Slug
+  authorId: string,
+}
 
-  constructor(title: string, content: string, id?: string) {
-    this.title = title,
-      this.content = content,
-      this.id = id ?? randomUUID()
-  }
+export class Question extends Entity<QuestionProps>{
+
 
 }
--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,9 +1,17 @@
 import {expect, test} from 'vitest'
 import { AnswerQuestionUseCase } from './answer-question'
+import { AnswersRepository } from '../repositories/answers-repository'
+import { Answer } from '../entities/answer'
+
+const fakeAnswerRepository:AnswersRepository={
+  async create(answer:Answer):Promise<void>{
+  return
+  }
+}
  
-test('create an answer', ()=>{
-  const answerQuestion = new AnswerQuestionUseCase()
-  const answer = answerQuestion.execute({
+test('create an answer', async ()=>{
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
+  const answer = await answerQuestion.execute({
     questionId:'1',
     instructorId:'1',
     content:'nova resposta'
