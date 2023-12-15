df1c9bea1025202db70b69dc4dfe0231025824f4
ernestopmaria <ernestomaria93@gmail.com>
2023-12-14 21:34:06 -0600

    testing fetch questions usecases

---
+++ src/domain/forum/application/use-cases/fetch-recent-topics.spec.ts
@@ -0,0 +1,51 @@
+import { InMemoryQuestionRepository } from "test/repositories/in-memory-questions-repository"
+import { CreateQuestionUseCase } from "./create-question"
+import { FetchRecentQuestionsUseCase } from "./fetch-recent-topics"
+import { makeQuestion } from "test/factories/make-question"
+
+
+let inMemoryQuestionRepository: InMemoryQuestionRepository
+let sut: FetchRecentQuestionsUseCase
+
+describe('Fetch questions', () => {
+  beforeEach(async () => {
+    inMemoryQuestionRepository = new InMemoryQuestionRepository(),
+      sut = new FetchRecentQuestionsUseCase(inMemoryQuestionRepository)
+  })
+
+  it('it should be able to fetch questions', async () => {
+
+    await inMemoryQuestionRepository.create(makeQuestion({
+      createdAt: new Date(2023, 11, 20)
+    }))
+    await inMemoryQuestionRepository.create(makeQuestion({
+      createdAt: new Date(2023, 11, 18)
+    }))
+    await inMemoryQuestionRepository.create(makeQuestion({
+      createdAt: new Date(2023, 11, 23)
+    }))
+
+    const { questions } = await sut.execute({
+      page: 1
+    })
+
+    expect(questions).toEqual([
+      expect.objectContaining({createdAt: new Date(2023, 11, 23) }),
+      expect.objectContaining({createdAt: new Date(2023, 11, 20) }),
+      expect.objectContaining({createdAt: new Date(2023, 11, 18) })
+    ])
+  })
+
+  it('it should be able to fetch paginated recents questions', async () => {
+
+    for (let i = 1; i<=22; i++) {
+      await inMemoryQuestionRepository.create(makeQuestion())      
+    }
+  
+     const { questions } = await sut.execute({
+      page: 2
+    })
+
+    expect(questions).toHaveLength(2)
+  })
+})
--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { Question } from "@/domain/enterprise/entities/question";
 import { QuestionsRepository } from "@/domain/forum/application/repositories/questions-repository";
 
@@ -22,6 +23,12 @@ export class InMemoryQuestionRepository implements QuestionsRepository {
       return null
     }
     return question
+  }
+
+  async findManyRecent({ page }: PaginationParams){
+    const questions = this.items.sort((a, b)=> b.createdAt.getTime() - a.createdAt.getTime()).slice(( page - 1 ) * 20, page * 20)
+
+    return questions
   }
 
   async create(question: Question) {
